0f27fe566f38301b41a6c3d2c26ae73437bf8b06
CarlosNobuaki <57003228+CarlosNobuaki@users.noreply.github.com>
2021-11-23 19:24:12 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "concessionaria";
+$host = "us-cdbr-east-04.cleardb.com";
+$user = "b244f66c3942ac";
+$pass = "1ccc39e8";
+$dbname = "heroku_7107136f272449a";
 
 
 $conn = mysqli_connect($host,$user,$pass,$dbname);
