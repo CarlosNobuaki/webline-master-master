26e27f735b07dde907499deb2cc177dbee894f5a
Carlos Nobuaki <hokumura32@gmail.com>
2022-10-25 20:21:36 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "us-cdbr-east-04.cleardb.com";
-$user = "b244f66c3942ac";
-$pass = "1ccc39e8";
-$dbname = "heroku_7107136f272449a";
+$host = "#######";
+$user = "#######";
+$pass = "#######";
+$dbname = "##########";
 
 
 $conn = mysqli_connect($host,$user,$pass,$dbname);
